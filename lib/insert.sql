0c7c36f5389c18ab7fed654e446e03032305384f
grant smith <grantsmith1256@gmail.com>
2019-01-12 19:44:16 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO series (title, author_id, sub_genre) VALUES
+("Harry Potter", 1, "Thriller"), ("The Chronicles of Narnia", 2, "Fantasy Fiction");
+
+INSERT INTO sub_genre (name) VALUES ("Thriller"), ("Fantasy Fiction");
+
+INSERT INTO authors (name) VALUES ("C. S. Lewis"), ("J. K. Rowling");
