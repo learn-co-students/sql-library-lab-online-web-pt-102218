e68d74841499f5bd22233060e3e049e7be2d4051
grant smith <grantsmith1256@gmail.com>
2019-01-12 19:54:01 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,3 +4,11 @@ INSERT INTO series (title, author_id, sub_genre) VALUES
 INSERT INTO sub_genre (name) VALUES ("Thriller"), ("Fantasy Fiction");
 
 INSERT INTO authors (name) VALUES ("C. S. Lewis"), ("J. K. Rowling");
+
+INSERT INTO books (title, year, series_id) VALUES
+("Harry Potter and the Chamber of Secrets", 1998, 1),
+("Harry Potter and the Prisoner of Azkaban", 1999, 1),
+("Harry Potter and the Order of the Phoenix", 2003, 1),
+("The Lion, the Witch and the Wardrobe", 1950, 2),
+("The Magician's Nephew", 1955 , 2),
+("The Last Battle", 1956, 2);
