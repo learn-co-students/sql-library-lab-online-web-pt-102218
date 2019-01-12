c369a2d19e129a93d1495082562f91db3d50ca37
grant smith <grantsmith1256@gmail.com>
2019-01-12 20:46:23 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,18 +1,14 @@
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES
-(1, "Harry Potter", 1, 1);
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES
+(1, "Harry Potter", 1, 1),
 (2, "The Chronicles of Narnia", 2, 2);
 
-INSERT INTO subgenres (id, name) VALUES (1, "Thriller"), (2, "Fantasy Fiction");
-
-INSERT INTO authors (id, name) VALUES (1, "J. K. Rowling"), (2, "C. S. Lewis");
 
 INSERT INTO books (id, title, year, series_id) VALUES
 (1, "Harry Potter and the Chamber of Secrets", 1998, 1),
 (2, "Harry Potter and the Prisoner of Azkaban", 1999, 1),
 (3, "Harry Potter and the Order of the Phoenix", 2003, 1),
 (4, "The Lion, the Witch and the Wardrobe", 1950, 2),
-(5, "The Magician's Nephew", 1955 , 2),
+(5, "The Magician's Nephew", 1955, 2),
 (6, "The Last Battle", 1956, 2);
 
 INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES
@@ -25,6 +21,15 @@ INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES
 (7, "Prince Caspian", "King of Narnia", "Human", 2, 2),
 (8, "Susan Pevensie", "Queen Susan the Gentle", "Human", 2, 2);
 
+
+INSERT INTO subgenres (id, name) VALUES (1, "Thriller"), (2, "Fantasy Fiction");
+
+INSERT INTO authors (id, name) VALUES (1, "J. K. Rowling"), (2, "C. S. Lewis");
+
+
+
 INSERT INTO character_books (id, character_id, book_id) VALUES
-(1, 1, (1, 2, 3)), (2, 2, (1, 2, 3)), (3, 3, 1), (4, 4, 3),
-(5, 5, (4, 5, 6)), (6, 6, (4, 5, 6)), (7, 7, 4), (8, 8, 6);
+(1, 1, 1), (2, 2, 1), (3, 3, 1), (4, 4, 3),
+(5, 5, 4), (6, 6, 4), (7, 7, 4), (8, 8, 6),
+(9, 1, 2), (10, 1, 3), (11, 2, 2), (12, 2, 3), (13, 5, 5), (14, 5, 6),
+(15, 6, 5), (16, 6, 6);
