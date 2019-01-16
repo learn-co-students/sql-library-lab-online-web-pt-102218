f891b4b98642b76fa4e750eda8c421fbd36df527
Johnathon Wood <jaywood128@gmail.com>
2019-01-16 16:28:39 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,20 +20,24 @@ def select_series_title_with_most_human_characters
   GROUP BY series.title ORDER BY COUNT(*) DESC LIMIT 1;"
 end
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(*) AS book_count FROM character_books JOIN characters ON character_books.character_id = characters.id
+  GROUP BY characters.name ORDER BY book_count DESC;"
 end
 # def select_series_title_with_most_human_characters
 #   <<-SQL
 #     SELECT series.title /* you are selecting the title column of series */
 #     FROM series
+
 #     JOIN books  /* need to join to books table */
 #     ON books.series_id = series.id  /* here is where to execute the join */
 #     JOIN character_books  /* also join character_books, the join table that associates books and characters */
 #     ON character_books.book_id = books.id /* where to join it */
 #     JOIN characters /* last join on characters */
+
 #     ON character_books.character_id = characters.id
 #     WHERE characters.species = 'human' /* where species is human */
 #     GROUP BY series.title
+
 #     ORDER BY COUNT(*) DESC /* here is where we can guarantee that the top one is the one with most human characters
 #     LIMIT 1 /* will grab just the top one, the most because we sorted descending */
 #   SQL
